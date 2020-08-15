69e4c0c9cfec8863e0e372804c22cfe523cdb513
Stylianos Kamnakis <kamnis1997@gmail.com>
2020-08-15 10:33:13 +0300

    changed cors settings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const cors = require('cors')
 require('./db/mongoose')
 
 const userRouter = require('./routers/user')
@@ -7,7 +7,10 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT
-
+app.use(cors({
+    origin: 'https://kamnakis.github.io/task-manager/',
+    optionsSuccessStatus: 200
+}))
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
