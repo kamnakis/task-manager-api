2670f72391de3254cf0f8133416e9fd0026af13c
Stylianos Kamnakis <kamnis1997@gmail.com>
2020-08-15 13:35:38 +0300

    Disable email send

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,21 +3,21 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'kamnis1997@gmail.com',
-        subject: 'Welcome to Task Manager',
-        text: `Dear ${name}. Welcome to Task Manager. Let me know how you get along with the app!`
-    })
+    // sgMail.send({
+    //     to: email,
+    //     from: 'kamnis1997@gmail.com',
+    //     subject: 'Welcome to Task Manager',
+    //     text: `Dear ${name}. Welcome to Task Manager. Let me know how you get along with the app!`
+    // })
 }
 
 const sendCancellationEmail = (email, name) => {
-    sgMail.send({
-        to: email,
-        from: 'kamnis1997@gmail.com',
-        subject: 'Sad to see you go',
-        text: `Dear ${name}. Hope to see you back soon!`
-    })
+    // sgMail.send({
+    //     to: email,
+    //     from: 'kamnis1997@gmail.com',
+    //     subject: 'Sad to see you go',
+    //     text: `Dear ${name}. Hope to see you back soon!`
+    // })
 }
 
 module.exports = { sendWelcomeEmail, sendCancellationEmail }
