be5eabaaaa9c05d1af56c6936cb46819b1935d6c
Stylianos Kamnakis <kamnis1997@gmail.com>
2020-08-15 13:24:06 +0300

    Added the /api in front of every route

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,7 +3,7 @@ const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/tasks', auth, async (req, res) => {
+router.post('/api/tasks', auth, async (req, res) => {
     const task = new Task({
         ...req.body,
         owner: req.user._id
@@ -18,7 +18,7 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 
-router.get('/tasks', auth, async (req, res) => {
+router.get('/api/tasks', auth, async (req, res) => {
     const match = {}
     const sort = {}
 
@@ -51,7 +51,7 @@ router.get('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks/:id', auth, async (req, res) => {
+router.get('/api/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
@@ -67,7 +67,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', auth, async (req, res) => {
+router.patch('/api/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -92,7 +92,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', auth, async (req, res) => {
+router.delete('/api/tasks/:id', auth, async (req, res) => {
     try {
         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id})
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,7 +8,7 @@ const { sendWelcomeEmail, sendCancellationEmail } = require('../emails/account')
 
 const router = new express.Router()
 
-router.post('/users', async (req, res) => {
+router.post('/api/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
@@ -21,7 +21,7 @@ router.post('/users', async (req, res) => {
     }
 })
 
-router.post('/users/login', async (req, res) => {
+router.post('/api/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
@@ -32,7 +32,7 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.post('/users/logout', auth, async (req, res) => {
+router.post('/api/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token
@@ -45,7 +45,7 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
-router.post('/users/logoutAll', auth, async (req, res) => {
+router.post('/api/users/logoutAll', auth, async (req, res) => {
     try {
         req.user.tokens = []
         await req.user.save()
@@ -56,11 +56,11 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 })
 
-router.get('/users/me', auth, async (req, res) => {
+router.get('/api/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.patch('/users/me', auth, async (req, res) => {
+router.patch('/api/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
 
@@ -80,7 +80,7 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 })
 
-router.delete('/users/me', auth, async (req, res) => {
+router.delete('/api/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
         sendCancellationEmail(req.user.email, req.user.name)
@@ -103,7 +103,7 @@ const avatar = multer({
     }
 })
 
-router.post('/users/me/avatar', auth, avatar.single('avatar'), async (req, res) => {
+router.post('/api/users/me/avatar', auth, avatar.single('avatar'), async (req, res) => {
     const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer() 
     req.user.avatar = buffer
 
@@ -113,13 +113,13 @@ router.post('/users/me/avatar', auth, avatar.single('avatar'), async (req, res) 
     res.status(400).send({ error: error.message })
 })
 
-router.delete('/users/me/avatar', auth, async (req, res) => {
+router.delete('/api/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.send()
 })
 
-router.get('/users/:id/avatar', async (req, res) => {
+router.get('/api/users/:id/avatar', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
 
